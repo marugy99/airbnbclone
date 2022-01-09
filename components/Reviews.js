589a8a299b74fe3e2f81cb1c0f10b6e2463de06d
marugy99 <marugy99@gmail.com>
2022-01-09 01:14:52 -0300

    Conditional rendering for reviews page

--- components/Reviews.js
+++ components/Reviews.js
@@ -2,28 +2,32 @@ import { urlFor } from "../utils";
 
 const Reviews = ({ data }) => {
   return (
-    <article className="reviews">
-      <h2>Reviews</h2>
-      <div className="review-container">
-        {data.map((review, index) => (
-          <section key={index}>
-            <header className="review-header">
-              <img
-                src={urlFor(review.traveller.mainImage.asset)}
-                alt="User avatar"
-              />
-              <div>
-                <p>
-                  <strong>{review.traveller.name}</strong>
-                </p>
-                <p>{review.rating}</p>
-              </div>
-            </header>
-            <p>{review.reviewDescription}</p>
-          </section>
-        ))}
-      </div>
-    </article>
+    <>
+      {data && (
+        <article className="reviews">
+          <h2>Reviews</h2>
+          <div className="review-container">
+            {data.map((review, index) => (
+              <section key={index}>
+                <header className="review-header">
+                  <img
+                    src={urlFor(review.traveller.mainImage.asset)}
+                    alt="User avatar"
+                  />
+                  <div>
+                    <p>
+                      <strong>{review.traveller.name}</strong>
+                    </p>
+                    <p>{review.rating}</p>
+                  </div>
+                </header>
+                <p>{review.reviewDescription}</p>
+              </section>
+            ))}
+          </div>
+        </article>
+      )}
+    </>
   );
 };
 
