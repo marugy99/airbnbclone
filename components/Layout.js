8b220bf3ee30e08940aec7525d46c85e4496ef30
marugy99 <marugy99@gmail.com>
2022-02-10 20:42:41 -0300

    Update dependencies

--- components/Layout.js
+++ components/Layout.js
@@ -13,16 +13,14 @@ const Layout = ({ children }) => {
       <Head>
         <title>Airbnb Clone By Maru</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;400;700&family=Roboto+Mono:wght@300&display=swap"
           rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-          crossorigin=""
         />
       </Head>
 
